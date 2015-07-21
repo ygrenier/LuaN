a21f11a569d3deabfc6e239190c063c78ab4ccf0
Yan Grenier <ygrenier@ygrenier.com>
2015-07-22 00:17:51 +0200

    Highlight and folding in the text editor.

---
+++ src/LuaStudio/EditorControl.xaml.cs
@@ -0,0 +1,103 @@
+﻿using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Folding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LuaStudio
+{
+    /// <summary>
+    /// Logique d'interaction pour EditorControl.xaml
+    /// </summary>
+    public partial class EditorControl : UserControl
+    {
+        public EditorControl()
+        {
+            InitializeComponent();
+
+            var sh = HighlightingManager.Instance.LoadHighlighter("Lua", "Lua files", new String[] { ".lua", ".wlua" });
+            var sh2 = HighlightingManager.Instance.GetDefinition("Lua");
+            sh2 = HighlightingManager.Instance.GetDefinition("lua");
+
+            teEditor.SyntaxHighlighting = sh;
+
+            teEditor.TextArea.TextEntered += TextArea_TextEntered;
+            teEditor.TextArea.TextEntering += TextArea_TextEntering;
+
+
+        }
+
+        void TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            
+        }
+
+        void TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            UpdateFolding();
+        }
+        class SecInfos
+        {
+            public HighlightedSection Section { get; set; }
+            public String Keyword { get; set; }
+            public String Caption { get; set; }
+            public int LineNumber { get; set; }
+        }
+        FoldingManager foldingManager;
+        private void UpdateFolding()
+        {
+            if(foldingManager==null)
+                foldingManager = FoldingManager.Install(teEditor.TextArea);
+            var doc = teEditor.Document;
+            DocumentHighlighter dh = new DocumentHighlighter(doc, teEditor.SyntaxHighlighting);
+            Stack<SecInfos> sections = new Stack<SecInfos>();
+            List<NewFolding> foldings = new List<NewFolding>();
+            for (int i = 1; i <= doc.LineCount; i++)
+            {
+                //var line = doc.GetLineByNumber(i);
+                var line = dh.HighlightLine(i);
+                var kws = line.Sections
+                    .Where(s => s.Color != null && s.Color.Name == "Keyword");
+                foreach (var kw in kws)
+                {
+                    var kn = doc.GetText(kw.Offset, kw.Length);
+                    if (kn == "function" || kn == "while")
+                    {
+                        sections.Push(new SecInfos() {
+                            Section = kw,
+                            Keyword = kn,
+                            LineNumber = i
+                        });
+                    }
+                    else if (kn == "end")
+                    {
+                        if (sections.Count > 0)
+                        {
+                            var s = sections.Pop();
+                            if (s.LineNumber < i)
+                            {
+                                foldings.Add(new NewFolding(s.Section.Offset+s.Section.Length, kw.Offset + kw.Length));
+                            }
+                        }
+                    }
+                }
+                foldingManager.UpdateFoldings(foldings.OrderBy(f => f.StartOffset), -1);
+                //FoldingManager.Uninstall(fManager);
+            }
+        }
+
+    }
+}
---
+++ src/LuaStudio/EditorExtensions.cs
@@ -0,0 +1,34 @@
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace LuaStudio
+{
+    public static class EditorExtensions
+    {
+        public static IHighlightingDefinition LoadHighlighter(this HighlightingManager manager, String name, String caption, IEnumerable<String> extensions)
+        {
+            IHighlightingDefinition result = null;
+            using (Stream s = typeof(EditorExtensions).Assembly.GetManifestResourceStream(String.Format("LuaStudio.Syntax.{0}.xshd", name)))
+            {
+                if (s != null)
+                {
+                    using (XmlReader reader = new XmlTextReader(s))
+                    {
+                        result = ICSharpCode.AvalonEdit.Highlighting.Xshd.
+                            HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                    }
+                }
+            }
+            if (result != null)
+            {
+                manager.RegisterHighlighting(caption, extensions.ToArray(), result);
+            }
+            return result;
+        }
+    }
+}
