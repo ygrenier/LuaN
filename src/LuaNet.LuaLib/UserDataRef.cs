a030c0ee10115ed45fba24c3dada5354f59cc02c
Yan Grenier <ygrenier@ygrenier.com>
2015-08-03 09:26:32 +0200

    UserDataRef managed with an hased index.

--- src/LuaNet.LuaLib/UserDataRef.cs
+++ src/LuaNet.LuaLib/UserDataRef.cs
@@ -10,8 +10,71 @@ namespace LuaNet.LuaLib
     /// </summary>
     class UserDataRef
     {
-        static int _NextRef = 1;
-        static Dictionary<IntPtr, UserDataRef> _DataRefs = new Dictionary<IntPtr, UserDataRef>();
+        class UserDataIndex
+        {
+            int _NextRef = 1;
+            IDictionary<IntPtr, UserDataRef> _DataRefs = new Dictionary<IntPtr, UserDataRef>();
+            IDictionary<SByte, IList<UserDataRef>> _DataIndex = new SortedDictionary<sbyte, IList<UserDataRef>>();
+            UserDataRef FindData(Object data, out IList<UserDataRef> lst, out SByte hash)
+            {
+                hash = (SByte)(data.GetHashCode() >> 24);
+                if (!_DataIndex.TryGetValue(hash, out lst))
+                    return null;
+                return lst.FirstOrDefault(u => u.Data == data);
+            }
+            public UserDataRef FindData(Object data)
+            {
+                SByte hash;
+                IList<UserDataRef> lst;
+                return FindData(data, out lst, out hash);
+            }
+            public UserDataRef FindRef(IntPtr udRef)
+            {
+                UserDataRef result;
+                if (_DataRefs.TryGetValue(udRef, out result))
+                    return result;
+                return null;
+            }
+            public UserDataRef Add(Object data)
+            {
+                SByte hash;
+                IList<UserDataRef> lst;
+                var result = FindData(data, out lst, out hash);
+                if (result == null)
+                {
+                    result = new UserDataRef()
+                    {
+                        Data = data,
+                        Ref = new IntPtr(_NextRef++),
+                        RefCount = 0
+                    };
+                    _DataRefs[result.Ref] = result;
+                    if (lst == null)
+                    {
+                        lst = new List<UserDataRef>();
+                        _DataIndex[hash] = lst;
+                    }
+                    lst.Add(result);
+                }
+                return result;
+            }
+            public bool Remove(IntPtr udRef)
+            {
+                var ud = FindRef(udRef);
+                if (ud == null) return false;
+                var hash = (SByte)(ud.Data.GetHashCode() >> 24);
+                IList<UserDataRef> lst;
+                if (_DataIndex.TryGetValue(hash, out lst))
+                {
+                    lst.Remove(ud);
+                    if (lst.Count == 0)
+                        _DataIndex.Remove(hash);
+                }
+                return true;
+            }
+        }
+
+        static UserDataIndex _Index = new UserDataIndex();
 
         /// <summary>
         /// Get a ref
@@ -19,17 +82,9 @@ namespace LuaNet.LuaLib
         public static UserDataRef GetUserData(Object data)
         {
             if (data == null) return null;
-            UserDataRef result = _DataRefs.Values.FirstOrDefault(u => u.Data == data);
-            if (result == null)
-            {
-                result = new UserDataRef
-                {
-                    Data = data,
-                    RefCount = 0,
-                    Ref = new IntPtr(_NextRef++)
-                };
-                _DataRefs[result.Ref] = result;
-            }
+            var result = _Index.FindData(data);
+            if(result== null)
+                result = _Index.Add(data);
             return result;
         }
 
@@ -38,10 +93,7 @@ namespace LuaNet.LuaLib
         /// </summary>
         public static UserDataRef GetUserDataFromRef(IntPtr dref)
         {
-            UserDataRef result = null;
-            if (_DataRefs.TryGetValue(dref, out result))
-                return result;
-            return null;
+            return _Index.FindRef(dref);
         }
 
         /// <summary>
@@ -59,7 +111,7 @@ namespace LuaNet.LuaLib
         {
             RefCount--;
             if (RefCount <= 0)
-                _DataRefs.Remove(this.Ref);
+                _Index.Remove(this.Ref);
         }
 
         /// <summary>
