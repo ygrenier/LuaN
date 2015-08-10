d8ace36595b761c92f758935c857ac93f2ac61c2
Yan Grenier <ygrenier@ygrenier.com>
2015-08-10 10:37:29 +0200

    Create a Lua host object for .Net Lua management.

---
+++ src/LuaN.Tests/LuaTest.cs
@@ -0,0 +1,81 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LuaN.Tests
+{
+    public class LuaTest
+    {
+
+        [Fact]
+        public void TestCreateFromEngine()
+        {
+            var mState = new Mock<ILuaState>();
+            var state = mState.Object;
+
+            var mEngine = new Mock<ILuaEngine>();
+            mEngine.Setup(e => e.NewState()).Returns(state);
+
+            using(var l=new Lua(mEngine.Object))
+            {
+                Assert.Same(state, l.State);
+            }
+
+            mState.Verify(s => s.LuaPushString("LUAN HOSTED"), Times.Once());
+            mState.Verify(s => s.LuaPushBoolean(true), Times.Once());
+            mState.Verify(s => s.LuaSetTable(state.RegistryIndex), Times.Once());
+            mState.Verify(s => s.Dispose(), Times.Once());
+
+            Assert.Throws<ArgumentNullException>(() => new Lua((ILuaEngine)null));
+        }
+
+        [Fact]
+        public void TestCreateFromState()
+        {
+            // Normal
+            var mState = new Mock<ILuaState>();
+            mState.Setup(s => s.LuaToBoolean(-1)).Returns(false);
+            var state = mState.Object;
+            using (var l = new Lua(state))
+            {
+                Assert.Same(state, l.State);
+            }
+            mState.Verify(s => s.LuaPushString("LUAN HOSTED"), Times.Exactly(2));   // Once to check, Once to defined
+            mState.Verify(s => s.LuaGetTable(state.RegistryIndex), Times.Once());
+            mState.Verify(s => s.LuaToBoolean(-1), Times.Once());
+            mState.Verify(s => s.LuaPushBoolean(true), Times.Once());
+            mState.Verify(s => s.LuaSetTable(state.RegistryIndex), Times.Once());
+            mState.Verify(s => s.Dispose(), Times.Once());
+
+            // State not owned
+            mState = new Mock<ILuaState>();
+            mState.Setup(s => s.LuaToBoolean(-1)).Returns(false);
+            state = mState.Object;
+            using (var l = new Lua(state, false))
+            {
+                Assert.Same(state, l.State);
+            }
+            mState.Verify(s => s.LuaPushString("LUAN HOSTED"), Times.Exactly(2));   // Once to check, Once to defined
+            mState.Verify(s => s.LuaGetTable(state.RegistryIndex), Times.Once());
+            mState.Verify(s => s.LuaToBoolean(-1), Times.Once());
+            mState.Verify(s => s.LuaPushBoolean(true), Times.Once());
+            mState.Verify(s => s.LuaSetTable(state.RegistryIndex), Times.Once());
+            mState.Verify(s => s.Dispose(), Times.Never()); // Because the state is not owned, there is not disposed
+
+            // State already hosted
+            mState = new Mock<ILuaState>();
+            mState.Setup(s => s.LuaToBoolean(-1)).Returns(true);
+            state = mState.Object;
+            var ioex = Assert.Throws<InvalidOperationException>(() => new Lua(state));
+            Assert.Equal("This state is already hosted.", ioex.Message);
+            mState.Verify(s => s.Dispose(), Times.Never());
+
+            Assert.Throws<ArgumentNullException>(() => new Lua((ILuaState)null));
+        }
+
+    }
+}
---
+++ src/LuaN/Lua.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LuaN
+{
+    /// <summary>
+    /// Lua state host
+    /// </summary>
+    public class Lua : IDisposable
+    {
+        bool _StateOwned;
+
+        #region Ctors & Dest. , Dispose
+
+        /// <summary>
+        /// Create a Lua host from an engine
+        /// </summary>
+        public Lua(ILuaEngine engine)
+        {
+            if (engine == null) throw new ArgumentNullException("engine");
+            // Create the state
+            State = engine.NewState();
+            _StateOwned = true;
+            InitState();
+        }
+
+        /// <summary>
+        /// Create a Lua host from a existing state
+        /// </summary>
+        /// <remarks>
+        /// Multiple Lua host can't share the same Lua state.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">When <paramref name="state"/> is null.</exception>
+        /// <exception cref="InvalidOperationException">When a lua state is already hosted.</exception>
+        public Lua(ILuaState state, bool ownState = true)
+        {
+            if (state == null) throw new ArgumentNullException("state");
+            // Check if the state is already hosted
+            state.LuaPushString("LUAN HOSTED");
+            state.LuaGetTable(state.RegistryIndex);
+            if (state.LuaToBoolean(-1))
+            {
+                state.LuaSetTop(-2);
+                throw new InvalidOperationException("This state is already hosted.");
+            }
+            else
+            {
+                state.LuaSetTop(-2);
+                State = state;
+                _StateOwned = ownState;
+                InitState();
+            }
+        }
+
+        /// <summary>
+        /// Destructor
+        /// </summary>
+        ~Lua()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Internal release resources
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (State != null)
+            {
+                if (_StateOwned)
+                    State.Dispose();
+                State = null;
+            }
+        }
+
+        /// <summary>
+        /// Release resources
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.WaitForPendingFinalizers();
+        }
+
+        #endregion
+
+        #region State management
+
+        /// <summary>
+        /// Initialize the state
+        /// </summary>
+        void InitState()
+        {
+            // Register state as hosted
+            State.LuaPushString("LUAN HOSTED");
+            State.LuaPushBoolean(true);
+            State.LuaSetTable(State.RegistryIndex);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Current state
+        /// </summary>
+        public ILuaState State { get; private set; }
+
+    }
+}
