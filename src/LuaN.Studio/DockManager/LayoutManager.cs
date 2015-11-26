5059069d9621a857dc46d2be1050de05a861c342
Yan Grenier <ygrenier@ygrenier.com>
2015-11-26 20:16:09 +0100

    Manage the dock layout. Move the interactive lua tool at the bottom side.

---
+++ src/LuaN.Studio/DockManager/LayoutManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xceed.Wpf.AvalonDock.Layout;
+
+namespace LuaN.Studio.DockManager
+{
+    public class LayoutManager : ILayoutUpdateStrategy
+    {
+        public void AfterInsertAnchorable(LayoutRoot layout, LayoutAnchorable anchorableShown)
+        {
+        }
+
+        public void AfterInsertDocument(LayoutRoot layout, LayoutDocument anchorableShown)
+        {
+        }
+
+        public bool BeforeInsertAnchorable(LayoutRoot layout, LayoutAnchorable anchorableToShow, ILayoutContainer destinationContainer)
+        {
+            var tool = anchorableToShow.Content as ViewModels.ToolViewModel;
+            if (tool != null)
+            {
+                AnchorSide pSide = AnchorSide.Right;
+                if (tool is ViewModels.IInteractiveLuaToolViewModel)
+                {
+                    pSide = AnchorSide.Bottom;
+                    anchorableToShow.AutoHideMinHeight = 350;
+                }
+                LayoutAnchorSide aSide = null;
+                switch (pSide)
+                {
+                    case AnchorSide.Left:
+                        aSide = layout.LeftSide;
+                        break;
+                    case AnchorSide.Top:
+                        aSide = layout.TopSide;
+                        break;
+                    case AnchorSide.Bottom:
+                        aSide = layout.BottomSide;
+                        break;
+                    case AnchorSide.Right:
+                    default:
+                        aSide = layout.RightSide;
+                        break;
+                }
+                var parentGroup = aSide.Children.FirstOrDefault();
+                if (parentGroup == null)
+                {
+                    parentGroup = new LayoutAnchorGroup();
+                    aSide.Children.Add(parentGroup);
+                }
+                parentGroup.Children.Add(anchorableToShow);
+                return true;
+            }
+            return false;
+        }
+
+        public bool BeforeInsertDocument(LayoutRoot layout, LayoutDocument anchorableToShow, ILayoutContainer destinationContainer)
+        {
+            return false;
+        }
+    }
+}
